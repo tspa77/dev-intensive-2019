7b6dbad582da326575dfc880aab6084d0c41de9c
Pavel <tspa77@gmail.com>
2019-06-30 21:11:50 +0700

    Hometask_2. Start

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,42 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+data class User(
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    val lastVisit: Date? = null,
+    val isOnline: Boolean = false
+) {
+    constructor(id: String, firstName: String?, lastName: String?) : this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    constructor(id: String) : this(id, "John", "Doe")
+
+    init {
+        println(
+            "It's Alive!!! ${if (lastName === "Doe") "His name is " +
+                    "$firstName $lastName" else "And his name is $firstName $lastName"}"
+        )
+    }
+
+    companion object Factory {
+        private var lasdId: Int = -1
+        fun makeUser(fullName: String?): User {
+            lasdId++
+            val (firstName, lastName) = Utils.parseFullName(fullName)
+            return User(id = "$lasdId", firstName = firstName, lastName = lastName)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        // TODO Возврщать ожидаемое значение
+
+        val parts: List<String>? = fullName?.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "Pavel Tsygankov"
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "PT"
+    }
+}
