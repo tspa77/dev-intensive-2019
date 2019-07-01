7be899e3ebe099e262c3a8887e24994d29a6d1b6
Pavel <tspa77@gmail.com>
2019-07-01 07:55:52 +0700

    Hometask_2. parseFullName - one test fails

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,13 +2,14 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        // TODO Возврщать ожидаемое значение
 
-        val parts: List<String>? = fullName?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
-        return firstName to lastName
+        return if (fullName.isNullOrBlank()) null to null else {
+            val parts: List<String>? = fullName?.trimIndent().split(" ")
+            val firstName = parts?.getOrNull(0)
+            val lastName = parts?.getOrNull(1)
+            firstName to lastName
+        }
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
