7ae4f050dc4b28220424010b14c55475c58bd750
Pavel <tspa77@gmail.com>
2019-07-04 23:50:02 +0700

    *String.stripHtml - убирает только тэги

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -5,3 +5,12 @@ fun String.truncate(length: Int = 16): String {
     if (stroka.length > length) stroka = "${stroka.substring(0, length).trimEnd()}..."
     return stroka
 }
+
+fun String.stripHtml(): String {
+
+    var string = this.replace("\\s+".toRegex(), " ")
+    string = string.replace("<[^>]*>".toRegex(), "")
+    println(string)
+
+    return string
+}
--- app/src/test/java/ru/skillbranch/devintensive/extensions/StringTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/extensions/StringTest.kt
@@ -26,11 +26,42 @@ class StringTest {
     @Test
     fun truncateTest() {
         /* admins tests */
-        assertEquals("123456...","123456789".truncate(6))
-        assertEquals("123456789","123456789".truncate(9))
-        assertEquals("Bender Bending R...","Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
-        assertEquals("Bender Bending...","Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
-        assertEquals("1","1     ".truncate(3))
+        assertEquals("123456...", "123456789".truncate(6))
+        assertEquals("123456789", "123456789".truncate(9))
+        assertEquals(
+            "Bender Bending R...",
+            "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate()
+        )
+        assertEquals(
+            "Bender Bending...",
+            "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15)
+        )
+        assertEquals("1", "1     ".truncate(3))
 
     }
+
+    /**
+     * *String.stripHtml
+     * Необходимо реализовать метод stripHtml для очистки строки от лишних пробелов, html тегов, escape
+     * последовательностей
+     * Реализуй extension позволяющий очистить строку от html тегов и html escape последовательностей
+     * ("& < > ' ""), а так же удалить пустые символы (пробелы) между словами если их больше 1. Необходимо
+     * вернуть модифицированную строку
+     * Пример:
+     * "<p class="title">Образовательное IT-сообщество Skill Branch</p>".stripHtml()
+     * //Образовательное IT-сообщество Skill Branch
+     * "<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml()
+     * //Образовательное IT-сообщество Skill Branch
+     */
+
+@Test
+    fun test_stripHtml(){
+        /* skillBranch tests */
+        assertEquals("Образовательное IT-сообщество Skill Branch",
+            "<p class=\"title\">Образовательное IT-сообщество Skill Branch</p>".stripHtml())
+        assertEquals("Образовательное IT-сообщество Skill Branch",
+            "<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml())
+
+    }
+
 }
