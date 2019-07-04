85f3930f9437d94eb0206b54ceacd18eb4045bf7
Pavel <tspa77@gmail.com>
2019-07-04 22:39:11 +0700

    *String.truncate - ready

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,21 +1,7 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(length: Int = 16): String {
-
-
-    var stroka = this
-    var rightBorder = length
-
-    if (this.length > length) {
-
-        println(rightBorder)
-        println("знак:${this[length - 1]}:")
-        if (this[length - 1] == ' ')          stroka = this.trimEnd()
-        println(rightBorder)
-        stroka = this.substring(0, rightBorder).plus("...")
-        println(stroka)
-        return stroka
-    }
-
-    return this.trimEnd()
+    var stroka = this.trim()
+    if (stroka.length > length) stroka = "${stroka.substring(0, length).trimEnd()}..."
+    return stroka
 }
