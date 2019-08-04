a4d442f0eed8228950fb53c90cddf86f3483643f
Pavel <tspa77@gmail.com>
2019-08-04 17:09:54 +0700

    mini fix

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -6,7 +6,11 @@ import ru.skillbranch.devintensive.utils.Utils
 
 fun User.toUserView(): UserView{
     val nickName = Utils.transliteration("$firstName $lastName")
-    val status = if (lastVisit == null) "Ещё ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit!!.humanizeDiff()}"
+    val status = when {
+        lastVisit == null -> "Ещё ни разу не был"
+        isOnline -> "online"
+        else -> "Последний раз был ${lastVisit!!.humanizeDiff()}"
+    }
     val initials = Utils.toInitials(firstName, lastName)
 
     return UserView(id, "$firstName $lastName", nickName, avatar, status, initials)
